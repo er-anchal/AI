3a014169abfdee0a5bd9f8f328a1201aba622ca2
Anchal Vaishya Banwarilal <er.anchalvaishya@gmail.com>
2026-05-15 18:13:01 +0530

    added

--- Backend/routes/pricingRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { getUserPricing } from "../controllers/pricingController.js";
-import { authMiddleware } from "../middleware/authMiddleware.js";
-
-
-router.get("/my-plan", authMiddleware, getUserPricing);
-
-export default router;
